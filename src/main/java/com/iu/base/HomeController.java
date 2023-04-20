b9bb84880e27a7c43a02326989bcbd5dc6575d5b
beaver10 <wkdgkdud1005@gmail.com>
2023-04-20 11:02:45 +0900

    1

--- src/main/java/com/iu/base/HomeController.java
+++ src/main/java/com/iu/base/HomeController.java
@@ -1,11 +1,23 @@
 package com.iu.base;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.iu.base.aoptest.Card;
+import com.iu.base.aoptest.Transport;
+import com.iu.base.board.BoardFileVO;
+import com.iu.base.util.Pager;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private Transport transport;
+	
+	@Autowired
+	private Card card;
 	
 	@GetMapping("/")
 	public String Home() {
@@ -13,5 +25,24 @@ public class HomeController {
 		return "index";
 		
 	}
+	
+	@GetMapping("/use")
+	public void use() throws Exception{
+		
+		Pager pager = new Pager();
+		pager.setKind("bus title");
+		
+		transport.useBus(pager);
+		
+		
+		BoardFileVO boardFileVO = new BoardFileVO();
+		boardFileVO.setFileName("subway file");
+		
+		transport.useSubway(boardFileVO);
+		
+		transport.takeWalk();
+		
+	}
+	
 
 }
