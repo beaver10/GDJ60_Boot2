0ffe1b5bbe26f326196119cb842a996fd0483989
beaver10 <wkdgkdud1005@gmail.com>
2023-05-02 15:15:27 +0900

    ec2 서버 변경

--- src/main/java/com/iu/base/security/UserLogoutSuccessHandler.java
+++ src/main/java/com/iu/base/security/UserLogoutSuccessHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -22,13 +23,18 @@ public class UserLogoutSuccessHandler implements LogoutSuccessHandler {
 	@Autowired
 	private MemberDAO memberDAO;
 	
+	@Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+	private String restKey;
+		
+	
 	@Override
 	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		
 		log.error("로그아웃 =============={}",memberDAO);
-		response.sendRedirect("/");
+		//response.sendRedirect("/");
+		response.sendRedirect("https://kauth.kakao.com/oauth/logout?client_id="+restKey+"&logout_redirect_uri=http://ec2-13-209-65-155.ap-northeast-2.compute.amazonaws.com/");
 		
 	}
 	
